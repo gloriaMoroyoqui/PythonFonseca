239ed81b6f7b39279942d0b366f601b8f4da81d6
Gloria Moroyoqui <gloriamoroyoqui8121@gmail.com>
2026-02-12 18:40:55 -0800

    Hice otro documento con otro codigo de lista

---
+++ sesion_02.py
@@ -0,0 +1,10 @@
+#Tuplas
+mi_tupla =(2,4)
+print ("Mi tupla:", mi_tupla)
+
+#Lista
+mi_lista = [1, 3.1416, "Gloria", mi_tupla]
+print("El primer elemento de mi lista:", mi_lista[0])
+print("El cuarto elemento de mi lista:", mi_lista[3])
+print("El Tercer elemento de mi lista:", mi_lista[2])
+      
