93d76957922b282da552ec6d641d61a386178584
Gloria Moroyoqui <gloriamoroyoqui8121@gmail.com>
2026-02-12 19:04:00 -0800

    Agregamos una lista

--- sesion_02.py
+++ sesion_02.py
@@ -8,3 +8,13 @@ print("El primer elemento de mi lista:", mi_lista[0])
 print("El cuarto elemento de mi lista:", mi_lista[3])
 print("El Tercer elemento de mi lista:", mi_lista[2])
       
+#Diccionario
+mi_diccionario = {
+    "mi_lista": mi_lista,
+    "nombre": "Gloria",
+    "pi": 3.1416,
+    "Tel": "664 749 9649"
+}
+print("Llave para accesar a mi diccionario mi_lista:", mi_diccionario["mi_lista"])
+print("Llave para accesar a mi diccionario pi:", mi_diccionario["pi"])
+print("Llave para accesar a mi diccionario Tel:", mi_diccionario["Tel"])
